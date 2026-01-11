786e7aec34ca863eb4c37b3f14ad14a9e083ed9b
Josh Lim <joshualim142@gmail.com>
2026-01-11 16:50:33 +0800

    Refactor database schema and add Curriculum, Prorgam, ProfessorProgram, and ActivityLog tables
    Renamed `Admin` table to `Account` and added columns for `FirstName`, `LastName`, and `UserType`. Created new tables: `Curriculum`, `Program`, `ProfessorProgram`, and `ActivityLog` to support curriculum, program, and activity tracking.
    
    Updated `Course` table to include `ProgramID` and renamed `CourseDecription` to `CourseDescription`. Expanded `Professor` table with a `Suffix` column and increased `MiddleName` length. Added foreign key constraints to enforce relationships between tables.
    
    Populated new tables with sample data and updated existing data to reflect schema changes. Added `SELECT` queries for new tables.

--- RMSDB_SQLQuery.sql
+++ RMSDB_SQLQuery.sql
@@ -2,23 +2,52 @@ CREATE DATABASE RMSDB;
 
 USE RMSDB;
 
-CREATE TABLE Admin(
+CREATE TABLE Account(
 	AdminID INT IDENTITY(1,1) PRIMARY KEY,
 	Username VARCHAR(50) UNIQUE NOT NULL,
-	Password VARCHAR(50) UNIQUE NOT NULL
+	Password VARCHAR(50) UNIQUE NOT NULL,
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	UserType VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Curriculum(
+	CurriculumID INT IDENTITY(1,1) PRIMARY KEY,
+	CurriculumYear VARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE Program(
+	ProgramID INT IDENTITY(1,1) PRIMARY KEY,
+	ProgramCode VARCHAR(20) NOT NULL,
+	ProgramDescription VARCHAR(50) NOT NULL,
+	CurriculumID INT
+	
+	CONSTRAINT FK_Curriculum_Program FOREIGN KEY (CurriculumID) REFERENCES Curriculum(CurriculumID)
 );
 
 CREATE TABLE Course(
 	CourseID INT IDENTITY(1,1) PRIMARY KEY,
-	CourseCode VARCHAR(10) NOT NULL,
-	CourseDecription VARCHAR(50) NOT NULL,
+	CourseCode VARCHAR(20) NOT NULL,
+	CourseDescription VARCHAR(50) NOT NULL,
+	ProgramID INT,
+
+	CONSTRAINT FK_Program_Course FOREIGN KEY (ProgramID) REFERENCES Program(ProgramID)
 );
 
 CREATE TABLE Professor(
 	ProfessorID INT IDENTITY(1,1) PRIMARY KEY,
 	FirstName VARCHAR(50) NOT NULL,
-	MiddleName VARCHAR(10),
-	LastName VARCHAR(50) NOT NULL
+	MiddleName VARCHAR(20),
+	LastName VARCHAR(50) NOT NULL,
+	Suffix VARCHAR(10)
+);
+
+CREATE TABLE ProfessorProgram(
+	ProfessorID INT,
+	ProgramID INT,
+
+	CONSTRAINT FK_ProfessorID_ProfessorProgram FOREIGN KEY (ProfessorID) REFERENCES Professor(ProfessorID),
+	CONSTRAINT FK_ProgramID_ProfessorProgram FOREIGN KEY (ProgramID) REFERENCES Program(ProgramID)
 );
 
 CREATE TABLE GradeSheet(
@@ -35,11 +64,33 @@ CREATE TABLE GradeSheet(
 	CONSTRAINT FK_AdminID_GradeSheet FOREIGN KEY (AdminID) REFERENCES Admin(AdminID)
 );
 
+CREATE TABLE ActivityLog(
+	LogID INT IDENTITY(1,1) PRIMARY KEY,
+	AdminID INT,
+	ActivityDescription VARCHAR(255) NOT NULL,
+	ActivityDate DATETIME,
+
+	CONSTRAINT FK_AdminID_ActivityLog FOREIGN KEY (AdminID) REFERENCES Admin(AdminID)
+);
+
 
 -- SAMPLE DATA
 
--- ADMIN Account
-INSERT INTO Admin(Username, Password) VALUES('user', '12345678');
+-- ACCOUNT
+INSERT INTO Admin(Username, Password) VALUES('user', '12345678', 'Bong', 'Montante', 'Admin');
+
+-- CURRICULUM
+INSERT INTO Curriculum(CurriculumYear) VALUES('2021-2025');
+
+-- PROGRAM
+INSERT INTO Program(ProgramCode, ProgramDescription, CurriculumID) VALUES('BSIT-LQ', 'Bachelor of Science in Information Technology', 1);
+
+-- COURSE
+INSERT INTO Course(CourseCode, CourseDecription, ProgramID) VALUES('COMP 015', 'Fundamentals of Research', 1);
+INSERT INTO Course(CourseCode, CourseDecription, ProgramID) VALUES('COMP 016', 'Web Development', 1);
+INSERT INTO Course(CourseCode, CourseDecription, ProgramID) VALUES('COMP 017', 'Multimedia', 1);
+INSERT INTO Course(CourseCode, CourseDecription, ProgramID) VALUES('COMP 018', 'Database Administration', 1);
+INSERT INTO Course(CourseCode, CourseDecription, ProgramID) VALUES('INTE 301', 'Systems Integration and Architecture 1', 1);
 
 -- PROFESSOR
 INSERT INTO Professor(FirstName, MiddleName, LastName) VALUES('Mark Vence', 'V' , 'Dungca');
@@ -48,13 +99,16 @@ INSERT INTO Professor(FirstName, MiddleName, LastName) VALUES('Icon', 'C' , 'Obm
 INSERT INTO Professor(FirstName, MiddleName, LastName) VALUES('Marie Andrea', 'E' , 'Zurbano');
 INSERT INTO Professor(FirstName, MiddleName, LastName) VALUES('Tito Ernesto', 'Z' , 'Loreto');
 
--- COURSE
-INSERT INTO Course(CourseCode, CourseDecription) VALUES('COMP 015', 'Fundamentals of Research');
-INSERT INTO Course(CourseCode, CourseDecription) VALUES('COMP 016', 'Web Development');
-INSERT INTO Course(CourseCode, CourseDecription) VALUES('COMP 017', 'Multimedia');
-INSERT INTO Course(CourseCode, CourseDecription) VALUES('COMP 018', 'Database Administration');
-INSERT INTO Course(CourseCode, CourseDecription) VALUES('INTE 301', 'Systems Integration and Architecture 1');
+-- PROFESSOR-PROGRAM
+INSERT INTO ProfessorProgram(ProfessorID, ProgramID) VALUES(1, 1);
+INSERT INTO ProfessorProgram(ProfessorID, ProgramID) VALUES(2, 1);
+INSERT INTO ProfessorProgram(ProfessorID, ProgramID) VALUES(3, 1);
+INSERT INTO ProfessorProgram(ProfessorID, ProgramID) VALUES(4, 1);
+INSERT INTO ProfessorProgram(ProfessorID, ProgramID) VALUES(5, 1);
 
 SELECT * FROM Admin;
+SELECT * FROM Curriculum;
+SELECT * FROM Program;
 SELECT * FROM Course;
 SELECT * FROM Professor;
+SELECT * FROM ProfessorProgram;
